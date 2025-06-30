837b164c17ba5738e3c080963af923cf33a35efe
nizamhasanov <nhasanov@email.sc.edu>
2025-06-30 11:56:05 -0400

    Changed generator_enum

--- modular_autoencoder/generator_enum.py
+++ modular_autoencoder/generator_enum.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from image_generators.sinusoidal_image_generator import SinusoidalImageGenerator
+from image_generators.AtomP6 import AtomP6
 
 class GENERATORS(Enum):
-    SINUSOID = SinusoidalImageGenerator
+   # SINUSOID = SinusoidalImageGenerator
+    ATOMP6 = AtomP6
